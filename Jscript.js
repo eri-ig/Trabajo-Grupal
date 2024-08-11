4ca2771f40e4e7e449925af9ab4b9a8f1ed98436
Alison Gonzalez <168144637+VioletDarklight@users.noreply.github.com>
2024-08-11 01:13:02 -0300

    Cambios de estilo, letras, correcciones  y ajustes de codigo, mejora de la estructura e implementacion de una tipografia y paleta de colores.

--- Jscript.js
+++ Jscript.js
@@ -39,22 +39,65 @@ if(filtradoBusqueda.length > 0) {
 document.addEventListener('DOMContentLoaded',function(){
 
     const SEMANAS =[
-        {name:'Semana 1', href: "#sem1",src:"./imagenes/card1.png",h3:"Semana 1",p:"En esta primera semana queremos presentarte la modalidad del curso y las primeras herramientas que vas a usar como desarrollador web."},
-        {name:'Semana 2',href: "#sem2",src:"./imagenes/card2.png",h3:"Semana 2",p:"En esta semana vas a profundizar en cómo dar estructura a una página web empleando la herramienta básica de HTML.Veremos sus distintas etiquetas y su uso."},
-        {name:'Semana 3',href: "#sem3",src:"./imagenes/card3.png",h3:"Semana 3",p:"La idea es que esta semana aprendas a aplicar reglas de estilo para lograr un mejor resultado visual en el contenido. Para lograr esto vamos a comenzar a trabajar con CSS."},
-        {name:'Semana 4',href: "#sem4",src:"./imagenes/card4.png",h3:"Semana 4",p:"Con Javascript (JS) comenzarás a ampliar las funcionalidades de una página web "},
-        ];
-        SEMANAS.forEach(item =>{
-            document.getElementById('weekcards').innerHTML+="<div class='col'>"+
-            "<div class='card' style=\"width: 18rem;\">"+
-              "<img src=\""+item.src+"\" class=\"card-img-top\" alt=\"...\">"+
-              "<div class=\"card-body\">"+
-                "<h3 class=\"card-title\">"+item.name+"</h3>"+
-                "<p class=\"card-text\">"+item.p+"</p>"+
-                "<button type='button' class='btn btn-success' onclick=\"window.location.href='" + item.href + "'\">Ver Más</button>"+
-              "</div>"+
+        {
+            name: 'Semana 1',
+            href: "#sem1",
+            src: "./imagenes/card1.png",
+            subtitulo: "Introducción a la programación",
+            p: "En esta primera semana queremos presentarte la modalidad del curso y las primeras herramientas que vas a usar como desarrollador web."
+        },
+        {
+            name: 'Semana 2',
+            href: "#sem2",
+            src: "./imagenes/card2.png",
+            subtitulo: "Fundamentos | HTML5 | Etiquetas",
+            p: "En esta semana vas a profundizar en cómo dar estructura a una página web empleando la herramienta básica de HTML.Veremos sus distintas etiquetas y su uso."
+        },
+        {
+            name: 'Semana 3',
+            href: "#sem3",
+            src: "./imagenes/card3.png",
+            subtitulo: "CSS | Estilos | Diseño",
+            p: "La idea es que esta semana aprendas a aplicar reglas de estilo para lograr un mejor resultado visual en el contenido. Para lograr esto vamos a comenzar a trabajar con CSS."
+        },
+        {
+            name: 'Semana 4',
+            href: "#sem4",
+            src: "./imagenes/card4.png",
+            subtitulo: "Javascript | DOM",
+            p: "Con Javascript (JS) comenzarás a ampliar las funcionalidades de una página web "
+        },
+    ];
+
+
+    /* SEMANAS.forEach(semana => {
+        document.getElementById('weekcards').innerHTML += "<div class='col'>" +
+            "<div class='card shadow p-3 mb-5 bg-body rounded-4' style=\"width: 18rem;\">" +
+            "<img src=\"" + semana.src + "\" class=\"card-img-top\" alt=\"...\">" +
+            "<div class=\"card-body\">" +
+            "<h3 class=\"card-title\">" + semana.name + "</h3>" +
+            "<p class=\"card-text\">" + semana.p + "</p>" +
+            "<button type='button' class='btn btn-success' onclick=\"window.location.href='" + semana.href + "'\">Ver Más</button>" +
+            "</div>" +
             "</div> </div>"
-        });/*Esto es para las tarjetas*/
+    }); */
+
+    SEMANAS.forEach(semana => {
+        document.getElementById('weekcards').innerHTML += ` 
+        <a href="${semana.href}" class="col text-decoration-none">
+            <div class='card shadow mb-5 bg-body rounded-4' style="width: 18rem;">
+                <img src="${semana.src}" class="card-img-top semana-img rounded-4" alt="...">
+                <div class="card-body">
+                    <h3 class="card-title">${semana.name}</h3>
+                    <span class="card-text subtitulo">${semana.subtitulo}</span>
+                    <p class="parrafo">${semana.p}</p>
+                </div>
+            </div>
+        </div>`
+    });
+
+
+    /*Esto es para las tarjetas*/
 
      /*
      Forma cavernícolamente larga:
